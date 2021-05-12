4093d68361e7256ff72ce41f6bb61cad7fd10cb5
Tim <xin2theblue@gmail.com>
2021-05-13 00:03:54 +0900

    Basic addition CUDA

---
+++ 01_CUDA_add.cu
@@ -0,0 +1,37 @@
+#include <iostream>
+
+//Compile Command: nvcc [filename].cu -o [filename].exe
+
+__global__ void SetIndex(int *A)
+{
+    int index = threadIdx.x + blockIdx.x*blockDim.x;
+    A[index] = index;
+}
+
+int main(void)
+{
+    int N = 20;
+    int *dev_A, *A;
+
+    //Allocate Host memory size
+    A = (int*) malloc(N*sizeof(int));
+
+    //Allocate Device memory size
+    cudaMalloc((void**) &dev_A, N*sizeof(int));
+
+    //Set device memory to 0
+    cudaMemset(dev_A, 0, N*sizeof(int));
+
+    //Run Kernel
+    SetIndex<<<N/5,5>>>(dev_A);
+
+    //Copy back results
+    cudaMemcpy(A, dev_A, N*sizeof(int), cudaMemcpyDeviceToHost);
+
+    //Verify
+    for (int n = 0; n < N; n++)
+    {
+        std::cout<<A[n] << "\n";
+    }
+
+}
