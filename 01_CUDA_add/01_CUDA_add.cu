b440e6fa53c2d43cbc93abd35fd3f7202a1f2fd9
Tim <xin2theblue@gmail.com>
2021-05-14 22:48:51 +0900

    add function

--- 01_CUDA_add/01_CUDA_add.cu
+++ 01_CUDA_add/01_CUDA_add.cu
@@ -2,15 +2,28 @@
 
 //Compile Command: nvcc [filename].cu -o [filename].exe
 
+__device__ int AddOne(int A)
+{
+    //Functions in CUDA or GPU code start with __device__
+    A += 1; 
+    return A;
+}
+
 __global__ void SetIndex(int *A)
 {
+    //GPU function that sets the array index value to index and then adds 1
     int index = threadIdx.x + blockIdx.x*blockDim.x;
     A[index] = index;
+    A[index] = AddOne(A[index]);
+
 }
 
 int main(void)
 {
+    //Set array size to 20
     int N = 20;
+
+    //Declare host and device array pointer
     int *dev_A, *A;
 
     //Allocate Host memory size
@@ -25,6 +38,9 @@ int main(void)
     //Run Kernel, Using (N/5) blocks with 5 threads per block
     SetIndex<<<N/5,5>>>(dev_A);
 
+    //Synchronizing is not necessary since cudaMemcpy step is next but it never hurts to do so
+    cudaDeviceSynchronize();
+
     //Copy back results
     cudaMemcpy(A, dev_A, N*sizeof(int), cudaMemcpyDeviceToHost);
 
@@ -35,5 +51,5 @@ int main(void)
     }
 
     //Erasing the CUDA array
-    cudaFree(A)
+    cudaFree(A);
 }
