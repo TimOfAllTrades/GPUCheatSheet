14a0215b6c638863c4b6578500ea1f03c0d9b082
Tim <xin2theblue@gmail.com>
2023-03-21 13:40:02 +0900

    updated ADD

--- 01_CUDA_add/01_CUDA_add.cu
+++ 01_CUDA_add/01_CUDA_add.cu
@@ -2,67 +2,56 @@
 
 //Compile Command: nvcc [filename].cu -o [filename].exe
 
-__device__ int AddOne(int A)
+__device__ int AddOne(int A)                                            //Functions in CUDA or GPU code start with __device__
 {
-    //Functions in CUDA or GPU code start with __device__
     A += 1; 
     return A;
 }
 
-__global__ void SetIndex(int *A)
+__global__ void SetIndex(int *A)                                        //GPU function that sets adds array index value to index and then adds 1
 {
-    //GPU function that sets adds array index value to index and then adds 1
-    int index = threadIdx.x + blockIdx.x*blockDim.x;
-    A[index] += index;
-    A[index] = AddOne(A[index]);
-
+    int index = threadIdx.x + blockIdx.x*blockDim.x;                    //Calculating the index of thread in the grid
+    A[index] += index;                                                  //Incrementing the current array value by the index value
+    A[index] = AddOne(A[index]);                                        //Also adding an additional 1
 }
-void Verify(int *A,int N)
+
+void Verify(int *A,int N)                                               //A typical C function with no output
 {
     for (int n=0;n < N; n++)
     {
-        std::cout<< A[n] << "\n";
+        std::cout<< A[n] << "\n";                                       //Iterating over all indices and outputting the result.
     }
 }
 
 int main(void)
 {
-    //Set array size to 20
-    int N = 20;
+    
+    int N = 20;                                                         //Set array size to 20
+    
+    int *dev_A, *A;                                                     //Declare host and device array pointer
 
-    //Declare host and device array pointer
-    int *dev_A, *A;
+    A = (int*) malloc(N*sizeof(int));                                   //Allocate Host memory size
 
-    //Allocate Host memory size
-    A = (int*) malloc(N*sizeof(int));
-
-    //Set some initial values to host array
-    for (int n = 0; n < N ; n++)
+    for (int n = 0; n < N ; n++)                                        //Set some initial values to host array
     {
-        A[n] = 1;
+        A[n] = 1;                                                       //Set all values to 1
     }
 
-    //Allocate Device memory size
-    cudaMalloc((void**) &dev_A, N*sizeof(int));
+    
+    cudaMalloc((void**) &dev_A, N*sizeof(int));                         //Allocate video memory for calculation
 
-    //Set device memory to 0
-    cudaMemset(dev_A, 0, N*sizeof(int));
+    cudaMemset(dev_A, 0, N*sizeof(int));                                //Set device memory to 0
 
-    //Copy host memory to device memory
-    cudaMemcpy(dev_A, A, N*sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(dev_A, A, N*sizeof(int), cudaMemcpyHostToDevice);        //Copy System memory to device memory.
 
-    //Run Kernel, Using (N/5) blocks with 5 threads per block
-    SetIndex<<<N/5,5>>>(dev_A);
+    SetIndex<<<N/5,5>>>(dev_A);                                         //Run Kernel, Using (N/5) blocks with 5 threads per block
 
-    //Synchronizing is not necessary since cudaMemcpy step is next but it never hurts to do so
-    cudaDeviceSynchronize();
+    cudaDeviceSynchronize();                                            //Synchronizing is not necessary since cudaMemcpy step is next but it never hurts to do so
 
-    //Copy back results
-    cudaMemcpy(A, dev_A, N*sizeof(int), cudaMemcpyDeviceToHost);
+    cudaMemcpy(A, dev_A, N*sizeof(int), cudaMemcpyDeviceToHost);        //Copy back results
 
-    //Verify
-    Verify(A,N);
+    
+    Verify(A,N);                                                        //Once the results are copied back, we will output it to see if the results match
 
-    //Erasing the CUDA array.  dev_A must be reallocated if to be used again.
-    cudaFree(dev_A);
+    cudaFree(dev_A);                                                    //Erasing the CUDA array.  dev_A must be reallocated if to be used again.
 }
